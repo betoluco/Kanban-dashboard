085f2fc05f5b9681aaad9639c20c4ac3cedcc418
Carlos Alberto Hurtado <betoluco@hotmail.com>
2024-11-22 18:37:00 +0000

    Change in DynamoDB key schema

--- API/kanban_task.py
+++ API/kanban_task.py
@@ -2,6 +2,6 @@ import graphene
 
 class KanbanTask(graphene.ObjectType):
     PK =  graphene.String()
+    SK =  graphene.String()
     title = graphene.String()
     description = graphene.String()
-    status = graphene.String()  # Example: "To Do", "In Progress", "Done"
