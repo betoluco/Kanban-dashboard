13e48ed1221853b2eff90bcdb6122c52b8282f2b
EC2 Default User <ec2-user@ip-172-31-43-179.ec2.internal>
2024-11-22 00:38:49 +0000

    add PK (primary key) field

--- API/kanban_task.py
+++ API/kanban_task.py
@@ -1,6 +1,7 @@
 import graphene
 
 class KanbanTask(graphene.ObjectType):
+    PK =  graphene.String()
     title = graphene.String()
     description = graphene.String()
     status = graphene.String()  # Example: "To Do", "In Progress", "Done"
