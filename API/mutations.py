e9a441d827cbac427dbdd9b037f1a5aa51895bfd
Carlos Alberto Hurtado <betoluco@hotmail.com>
2024-11-22 20:42:45 +0000

    Add mutation to create and delete an items in DynamoDB table

--- API/mutations.py
+++ API/mutations.py
@@ -1,43 +1,67 @@
 import boto3
 import graphene
 import os
-import random
 import json
+from datetime import datetime
 
 from kanban_task import KanbanTask 
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+dynamodb = boto3.client('dynamodb')
 
 class CreateKanbanTask(graphene.Mutation):
     class Arguments:
         PK = graphene.String()
         title = graphene.String()
         description = graphene.String()
-        status = graphene.String()
     
     ok = graphene.Boolean()
     kanban_task = graphene.Field(lambda: KanbanTask)
     
-    def mutate(root, info, title, description, status):
+    def mutate(root, info, PK, title, description):
+        SK = str(datetime.now())
         try:
-            PK = title + str(random.randrange(1000000))
-            response = table.put_item(Item= {
-                'PK':PK,
-                'title':title, 
-                'description':description, 
-                'status':status 
-            })
+            response = dynamodb.put_item(
+                TableName=os.environ['DYNAMODB_TABLE'],
+                Item={
+                    'PK': {'S': PK},
+                    'SK': {'S': SK},
+                    'title': {'S': title},
+                    'description': {'S': description}
+                }
+            )
             kanban_task = KanbanTask(
                 PK=PK,
+                SK=SK,
                 title=title, 
                 description=description, 
-                status=status
             )
             ok = True
             return CreateKanbanTask(kanban_task=kanban_task, ok=ok)
         except Exception as e:
             print(f"Error scanning table: {str(e)}")
+            
+            
+class DeleteKanbanTask(graphene.Mutation):
+    class Arguments:
+        PK = graphene.String()
+        SK = graphene.String()
+    
+    ok = graphene.Boolean()
+    
+    def mutate(root, info, PK, SK):
+        try:
+            response = dynamodb.delete_item(
+                TableName=os.environ['DYNAMODB_TABLE'], 
+                Key={
+                    'PK': {'S': PK}, 
+                    'SK': {'S': SK}
+                }
+            )
+            ok = True
+            return DeleteKanbanTask(ok=ok)
+        except Exception as e:
+            print(f"Error scanning table: {str(e)}")
 
 class KanbanTaskMutation(graphene.ObjectType):
     create_kanban_task = CreateKanbanTask.Field()
+    delete_kanban_task = DeleteKanbanTask.Field()
